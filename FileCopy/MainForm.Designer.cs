281549b9c6bccfa614c58b572c329fb7b4118d77
aiming.wang <aiming.wang@fmchexian.com>
2017-08-13 21:49:30 +0800

    导入

---
+++ FileCopy/MainForm.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace FileCopy
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.tbFolderPath = new System.Windows.Forms.TextBox();
+            this.lbFolderTitle = new System.Windows.Forms.Label();
+            this.btnFindFileType = new System.Windows.Forms.Button();
+            this.rtbContent = new System.Windows.Forms.RichTextBox();
+            this.btnSelect2 = new System.Windows.Forms.Button();
+            this.tbFolderPath2 = new System.Windows.Forms.TextBox();
+            this.lbFolderTitle2 = new System.Windows.Forms.Label();
+            this.btnCoptFile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Location = new System.Drawing.Point(598, 52);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(75, 23);
+            this.btnSelect.TabIndex = 0;
+            this.btnSelect.Text = "选择";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // tbFolderPath
+            // 
+            this.tbFolderPath.Location = new System.Drawing.Point(120, 52);
+            this.tbFolderPath.Name = "tbFolderPath";
+            this.tbFolderPath.ReadOnly = true;
+            this.tbFolderPath.Size = new System.Drawing.Size(472, 21);
+            this.tbFolderPath.TabIndex = 1;
+            // 
+            // lbFolderTitle
+            // 
+            this.lbFolderTitle.Location = new System.Drawing.Point(52, 50);
+            this.lbFolderTitle.Name = "lbFolderTitle";
+            this.lbFolderTitle.Size = new System.Drawing.Size(62, 23);
+            this.lbFolderTitle.TabIndex = 2;
+            this.lbFolderTitle.Text = "文件夹：";
+            this.lbFolderTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnFindFileType
+            // 
+            this.btnFindFileType.Location = new System.Drawing.Point(120, 563);
+            this.btnFindFileType.Name = "btnFindFileType";
+            this.btnFindFileType.Size = new System.Drawing.Size(94, 23);
+            this.btnFindFileType.TabIndex = 3;
+            this.btnFindFileType.Text = "查找文件类型";
+            this.btnFindFileType.UseVisualStyleBackColor = true;
+            this.btnFindFileType.Click += new System.EventHandler(this.btnFindFileType_Click);
+            // 
+            // rtbContent
+            // 
+            this.rtbContent.BackColor = System.Drawing.Color.White;
+            this.rtbContent.Location = new System.Drawing.Point(120, 138);
+            this.rtbContent.Name = "rtbContent";
+            this.rtbContent.Size = new System.Drawing.Size(553, 406);
+            this.rtbContent.TabIndex = 4;
+            this.rtbContent.Text = "";
+            // 
+            // btnSelect2
+            // 
+            this.btnSelect2.Location = new System.Drawing.Point(598, 88);
+            this.btnSelect2.Name = "btnSelect2";
+            this.btnSelect2.Size = new System.Drawing.Size(75, 23);
+            this.btnSelect2.TabIndex = 0;
+            this.btnSelect2.Text = "选择";
+            this.btnSelect2.UseVisualStyleBackColor = true;
+            this.btnSelect2.Click += new System.EventHandler(this.btnSelect2_Click);
+            // 
+            // tbFolderPath2
+            // 
+            this.tbFolderPath2.Location = new System.Drawing.Point(120, 88);
+            this.tbFolderPath2.Name = "tbFolderPath2";
+            this.tbFolderPath2.ReadOnly = true;
+            this.tbFolderPath2.Size = new System.Drawing.Size(472, 21);
+            this.tbFolderPath2.TabIndex = 1;
+            // 
+            // lbFolderTitle2
+            // 
+            this.lbFolderTitle2.Location = new System.Drawing.Point(36, 86);
+            this.lbFolderTitle2.Name = "lbFolderTitle2";
+            this.lbFolderTitle2.Size = new System.Drawing.Size(78, 23);
+            this.lbFolderTitle2.TabIndex = 2;
+            this.lbFolderTitle2.Text = "复制文件夹：";
+            this.lbFolderTitle2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnCoptFile
+            // 
+            this.btnCoptFile.Location = new System.Drawing.Point(231, 563);
+            this.btnCoptFile.Name = "btnCoptFile";
+            this.btnCoptFile.Size = new System.Drawing.Size(75, 23);
+            this.btnCoptFile.TabIndex = 5;
+            this.btnCoptFile.Text = "复制文件";
+            this.btnCoptFile.UseVisualStyleBackColor = true;
+            this.btnCoptFile.Click += new System.EventHandler(this.btnCoptFile_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(749, 618);
+            this.Controls.Add(this.btnCoptFile);
+            this.Controls.Add(this.rtbContent);
+            this.Controls.Add(this.btnFindFileType);
+            this.Controls.Add(this.lbFolderTitle2);
+            this.Controls.Add(this.lbFolderTitle);
+            this.Controls.Add(this.tbFolderPath2);
+            this.Controls.Add(this.btnSelect2);
+            this.Controls.Add(this.tbFolderPath);
+            this.Controls.Add(this.btnSelect);
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.TextBox tbFolderPath;
+        private System.Windows.Forms.Label lbFolderTitle;
+        private System.Windows.Forms.Button btnFindFileType;
+        private System.Windows.Forms.RichTextBox rtbContent;
+        private System.Windows.Forms.Button btnSelect2;
+        private System.Windows.Forms.TextBox tbFolderPath2;
+        private System.Windows.Forms.Label lbFolderTitle2;
+        private System.Windows.Forms.Button btnCoptFile;
+    }
+}
+
---
+++ FileCopy/MainForm.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace FileCopy
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            var folder = new FolderBrowserDialog();
+            if (folder.ShowDialog() == DialogResult.OK)
+            {
+                tbFolderPath.Text = folder.SelectedPath;
+            }
+        }
+
+        private void btnFindFileType_Click(object sender, EventArgs e)
+        {
+            string folderPath = tbFolderPath.Text.Trim();
+            if (string.IsNullOrWhiteSpace(folderPath))
+            {
+                MessageBox.Show("请选择文件夹！", "温馨提示！");
+                return;
+            }
+            //
+            var fileList = new List<string>();
+            GetAllFile(folderPath, fileList);
+            var extensionList = new List<string>();
+            string extensionName;
+            foreach (var file in fileList)
+            {
+                extensionName = Path.GetExtension(file).ToLower();
+                if (extensionName.Length == 0 || extensionList.Contains(extensionName)) continue;
+                //
+                extensionList.Add(extensionName);
+            }
+            rtbContent.Text = string.Join("\n", extensionList);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="folderPath"></param>
+        /// <param name="fileList"></param>
+        private void GetAllFile(string folderPath, IList<string> fileList)
+        {
+            string[] folders = Directory.GetDirectories(folderPath);//得到所有子目录
+            foreach (string folder in folders)
+            {
+                GetAllFile(folder, fileList);//对每一个字目录做与根目录相同的操作：即找到子目录并将当前目录的文件名存入List
+            }
+            string[] files = Directory.GetFiles(folderPath);
+            foreach (string file in files)
+            {
+                fileList.Add(file);//将当前目录中的所有文件全名存入文件List
+            }
+        }
+
+        private void btnSelect2_Click(object sender, EventArgs e)
+        {
+            var folder = new FolderBrowserDialog();
+            if (folder.ShowDialog() == DialogResult.OK)
+            {
+                tbFolderPath2.Text = folder.SelectedPath;
+            }
+        }
+
+        private void btnCoptFile_Click(object sender, EventArgs e)
+        {
+            string folderPath = tbFolderPath.Text.Trim();
+            if (string.IsNullOrWhiteSpace(folderPath))
+            {
+                MessageBox.Show("请选择文件夹！", "温馨提示！");
+                return;
+            }
+            //
+            string folderPath2 = tbFolderPath2.Text.Trim();
+            if (string.IsNullOrWhiteSpace(folderPath2))
+            {
+                MessageBox.Show("请选择复制文件夹！", "温馨提示！");
+                return;
+            }
+            //
+            var extensions = new Dictionary<string, string>()
+            {
+                
+            };
+            var thread = new Thread(() =>
+            {
+                CopyFile(folderPath, folderPath2, folderPath, extensions);
+                MessageBox.Show("复制文件夹成功！", "温馨提示！");
+            });
+            thread.Start();
+        }
+
+        private void CopyFile(string fromPath, string copyFolderPath, string folderPath, Dictionary<string, string> extensions)
+        {
+            string[] folders = Directory.GetDirectories(folderPath);//得到所有子目录
+            string newFolder;
+            foreach (string folder in folders)
+            {
+                newFolder = folder.Replace(fromPath, copyFolderPath);
+                if (!Directory.Exists(newFolder))
+                    Directory.CreateDirectory(newFolder);
+                //
+                CopyFile(fromPath, copyFolderPath, folder, extensions);
+            }
+            //
+            string[] files = Directory.GetFiles(folderPath);
+            string extensionName, newFile;
+            bool contains;
+            foreach (string file in files)
+            {
+                newFile = file.Replace(fromPath, copyFolderPath);
+                extensionName = Path.GetExtension(file).ToLower();
+                contains = extensions.ContainsKey(extensionName);
+                if (contains)
+                    newFile = newFile + extensions[extensionName];
+                //
+                if (File.Exists(newFile)) continue;
+                //
+                if (contains)
+                {
+                    File.AppendAllText(newFile, File.ReadAllText(file, Encoding.UTF8), Encoding.UTF8);
+                }
+                //直接复制
+                else
+                {
+                    File.Copy(file, newFile);
+                }
+            }
+        }
+    }
+}
